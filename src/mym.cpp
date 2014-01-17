datajoint/mym

commit a705b6184852906e6349397669d29f9b19a14133

Fancyprint can crop datatype to 32 bits

src/mym.cpp
@@ -163,7 +163,7 @@ static void fancyprint(MYSQL_RES*res) {
     char**fmt = (char**) mxMalloc(nfield*sizeof(char*));
     for (ulong j = 0; j<nfield; j++) {
         fmt[j] = (char*) mxCalloc(10, sizeof(char));
-        sprintf(fmt[j], "  %%-%ds ", len[j]);
+        sprintf(fmt[j], "  %%-%lu ", len[j]);
     }
     /************************************************************************/
     //  Now print the actual data

commit 761a4741bac45970e1a735ec082eb76ddbaab4a1

Restore Fancyprint
* Incorrect format specifier introduced in a705b61

src/mym.cpp
@@ -163,7 +163,7 @@ static void fancyprint(MYSQL_RES*res) {
     char**fmt = (char**) mxMalloc(nfield*sizeof(char*));
     for (ulong j = 0; j<nfield; j++) {
         fmt[j] = (char*) mxCalloc(10, sizeof(char));
-        sprintf(fmt[j], "  %%-%lu ", len[j]);
+        sprintf(fmt[j], "  %%-%lus ", len[j]);
     }
     /************************************************************************/
     //  Now print the actual data