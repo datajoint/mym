f9b4b02b70a2d33cf32f902555d9b47b7fd97980
Saumil Patel <spatel@bcm.edu>
2018-04-11 15:19:45 -0500

    Fixed the mexing code for linux Tested the mym function on ubuntu 16.04 and Matlab 2018a

--- mex_compilation/compile_linux.m
+++ mex_compilation/compile_linux.m
@@ -1,6 +1,6 @@
 function compile_linux()
 % Build script for MyM (32-bit and 64-bit Linux)
-
+    
 mym_base = fileparts(fileparts(mfilename('fullpath')));
 mym_src = fullfile(mym_base, 'src');
 build_out = fullfile(mym_base, 'build', mexext());
@@ -11,6 +11,35 @@ mysql_base = fullfile(mym_base, 'mysql-connector');
 mysql_include = fullfile(mysql_base, 'include');
 mysql_platform_include = fullfile(mysql_base, ['include_' mexext()]);
 mysql_lib = fullfile(mysql_base, ['lib_' mexext()]);
+
+
+% check if mysql_config is availalble, if so, copy the .so files from location where package is installed
+[status,result]=system('mysql_config --libs') ;
+if (status==0)
+    idx = strfind(result, '-L') 
+    if ~isempty(idx)
+        idx1 = strfind(result, ' ') 
+        if ~isempty(idx1)
+            idx1 = idx1(1) ;
+            src_dir = result(idx+2:idx1-1) ;
+            cbuf = sprintf('cp %s/libmysqlclient.so* %s',src_dir,mysql_lib) 
+            [s1,~]=system(cbuf);
+            if (s1 ~= 0)
+                disp('Unable to copy libmysqlclient.so.* to mym mysql-connector folder') ;
+                return ;
+            end
+        else
+            disp('Source folder for libmysqlclient.so.* cannot be found') ;
+            return ;
+        end
+    else
+        disp('mysql_config returned an empty string, error may have occurred during mysqlclient-dev package installation') ;
+        return ;
+    end
+else
+    disp('Install mysqlclient-dev package before running this function') ;
+    return ;
+end
 
 lib = fullfile(mym_base, 'lib', mexext());
 
@@ -27,11 +56,11 @@ mex( ...
     sprintf('-L"%s"', mysql_lib), ...
     sprintf('-L"%s"', lib), ...
 	'-lmysqlclient', ...
-    '-Wl,-rpath,''\$ORIGIN''', '-Wl,-z,origin', ...
-	'-lz', ...
+ 	'-lz', ...
     fullfile(mym_src, 'mym.cpp'));
 
 % Pack mex with all dependencies into distribution directory
 copyfile(['mym.' mexext()], distrib_out);
 copyfile(fullfile(lib, '*'), distrib_out);
 copyfile(fullfile(mysql_lib, 'libmysqlclient.so*'), distrib_out);
+copyfile([mym_base '/readme_linux.txt'],distrib_out) ;
---
+++ readme_linux.txt
@@ -0,0 +1,6 @@
+To run the mex function on a linux matlab, make sure that the proper version of libmysqlclient.so is installed on the system via 
+sudo apt-get install libmysqlclient-dev
+
+The version of system .so files should match the ones with which the mym function was built
+
+The version of .so files with which mym was built are in the mexa64 distribution folder
