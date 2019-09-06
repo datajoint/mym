40cb8f0a7784e08000f06026767c7a0780d8f850
guzman-raphael <raphael.h.guzman@gmail.com>
2019-09-06 09:57:00 -0500

    Update to next minor version and update LNX x64 binary.

--- src/mym.h
+++ src/mym.h
@@ -33,8 +33,8 @@
 
 // mym version information
 #define MYM_VERSION_MAJOR 2
-#define MYM_VERSION_MINOR 6
-#define MYM_VERSION_BUGFIX 8
+#define MYM_VERSION_MINOR 7
+#define MYM_VERSION_BUGFIX 0
 
 
 // some local defintion
