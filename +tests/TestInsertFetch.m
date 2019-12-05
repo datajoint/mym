51906feef7bfbe6e832ddc4b9995259bd1e4ebc3
guzman-raphael <raphael.h.guzman@gmail.com>
2019-12-05 13:21:15 -0600

    Check python deserialization on use cases.

--- +tests/TestInsertFetch.m
+++ +tests/TestInsertFetch.m
@@ -9,9 +9,9 @@ classdef TestInsertFetch < tests.Prep
                 testCase.CONN_INFO.password, 'false');
             mym(curr_conn, 'create database `djtest_insert`;');
 
-            testCase.check(curr_conn, 'varchar(7)','S','raphael');
-            testCase.check(curr_conn, 'longblob','M',int64([1;2]));
-            testCase.check(curr_conn, 'varchar(8)','S','ýýýý');
+            testCase.check(curr_conn, 'varchar(7)','','S','raphael');
+            testCase.check(curr_conn, 'longblob','','M',int64([1;2]));
+            testCase.check(curr_conn, 'varchar(8)','','S','ýýýý');
 
             data = '1d751e2e-1e74-faf8-4ab4-85fde8ef72be';
             data = strrep(data, '-', '');
@@ -21,7 +21,7 @@ classdef TestInsertFetch < tests.Prep
             hexMtx = reshapedString.';
             decMtx = hex2dec(hexMtx);
             v = uint8(decMtx)';
-            testCase.check(curr_conn, 'binary(16)','B',v);
+            testCase.check(curr_conn, 'binary(16)','uuid','B',v);
 
             data = '1d751e2e-1e74-faf8-4ab4-85fde8ef72be';
             data = strrep(data, '-', '');
@@ -32,18 +32,25 @@ classdef TestInsertFetch < tests.Prep
             decMtx = hex2dec(hexMtx);
             v = uint8(decMtx);
             v_char = char(v)';
-            testCase.check(curr_conn, 'varchar(24)','S',v_char);
+            testCase.check(curr_conn, 'varchar(24)','','S',v_char);
 
             mym('closeall');
         end
     end
     methods (Static)
-        function check(conn_id, datatype, flag, data)
-            mym(conn_id, ['create table `djtest_insert`.`test_' datatype ...
-                '` (id int, name ' datatype ' comment ":' datatype ':");']);
-            mym(conn_id, ['INSERT INTO `djtest_insert`.`test_' datatype ...
+        function check(conn_id, mysql_datatype, dj_datatype, flag, data)
+            table_name = ['test_' strrep(mysql_datatype, '(', '')];
+            table_name = strrep(table_name, ')', '');
+            if dj_datatype
+                dj_type_comment = [' comment ":' dj_datatype ':"'];
+            else
+                dj_type_comment = [];
+            end
+            mym(conn_id, ['create table `djtest_insert`.`' table_name ...
+                '` (id int, name ' mysql_datatype dj_type_comment ');']);
+            mym(conn_id, ['INSERT INTO `djtest_insert`.`' table_name ...
                 '` (`id`,`name`) VALUES (0,"{' flag '}") '],data);
-            res = mym(conn_id, ['select * from `djtest_insert`.`test_' datatype '`']);
+            res = mym(conn_id, ['select * from `djtest_insert`.`' table_name '`']);
             ret = res.name{1};
             assert(all(ret == data));
         end
